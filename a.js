d7cd9113845e2a7e769a5b6673865b1ab32a1104
muhammdnaeemmadni <76472888+muhammdnaeemmadni@users.noreply.github.com>
2021-01-24 00:47:57 +0500

    Add files via upload

---
+++ a.js
@@ -0,0 +1,130 @@
+
+
+
+// chp #1
+// alert("Error!Please enter a valid password")
+
+// chp #1
+// alert("Welcome to JS Land...\nHappy Codding!") 
+
+// chp #1
+// alert("Welcome to JS Land...")
+// chp #1s
+// alert("Hello...I can run JS through my web browser's console")
+
+
+// chp #2
+// alert("Muhammad Naeem")
+
+// chp #2
+// alert("18 years old")
+
+// chp #2
+// alert("Certified Mobile Appliction Development")
+
+// chp #2
+// alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
+
+// chp #2
+// var mail ="muhammad naeem ghouri 999 @ gmail .com"
+// alert("My email address is" + "" + mail)
+
+
+// chp #2
+// alert("I am trying to learn from the Book A smarter way to learn JavaScript ")
+
+// chp #2
+// var You ="Yah! i can writeHTML contant through javascript"
+// document.write("Yah! i can writeHTML contant through javascript")
+
+// chp #4
+// var nn = "-----------------"
+// var mm = "$$$$$$$$"
+// var gg = "-----------------"
+// alert(nn+mm+gg)
+
+
+// chp #3
+// var birth ="I am 18 years old"
+// alert(birth)
+
+// chp #3
+// var site ="You have visited this site 14 times"
+// alert(site)
+
+
+// chp #3
+// var bir="my birtday year is 2002" 
+// document.write("my birtday year is 2002") 
+
+
+// var tow="date type of my ddeclared variable is number"
+// document.write("date type of my ddeclared variable is number")
+
+
+
+
+// chp #3
+// var visited ='Naeem'
+// var Product ='Paint'
+// var maal ='999'
+// document.write("Name" +" "+ visited,"Product" +" "+ Product, "Quantity" +" "+ maal)
+
+// chp #5
+
+// var nom = 3
+// var sum = 5
+// document.write("Sum of" +""+ nom, "and" +""+ sum,"is" +""+ eval(nom+sum))
+
+
+// var nom = 9
+// var sum = 5
+// document.write("Sum of" +""+ nom, "and" +""+ sum,"is" +""+ eval(nom-sum))
+
+
+// var nom = 20
+// var sum = 10
+// document.write("Sum of" +""+ nom, "and" +""+ sum,"is" +""+ eval(nom/sum))
+
+
+// var nom = 9
+// var sum = 5
+// document.write("Sum of" +""+ nom, "and" +""+ sum,"is" +""+ eval(nom*sum))
+
+// chp #5
+
+// var no = 600
+// var mlt = 5
+// document.write("Total Tickets Buy" +""+ mlt,"Tiket to movie is" +""+ eval(no*mlt),"PKR",) 
+
+// chp #5
+// var tab =4 
+// for (let i = 1 ; i <= 10 ; i++) {
+// document.write(tab +'*'+ i +"="+ eval(tab*i)+"<br/>")}
+
+// chp #5
+// var Celsius =10
+// var Fahrenheit =25
+// document.write(Celsius + "<sup> 0 </sup> C is" + (Fahrenheit-10)*5/9 +'<sup> 0 </sup> F' +'<br/>' +'<br/>'  )
+// document.write(Fahrenheit + "<sup> 0 </sup> F is" + (Celsius*5/9)+32 +'<sup> 0 </sup> C')
+
+
+// chp #6
+// var Pri = 12
+// var Pric = 15
+// var Or = 1
+// var Ord = 2
+// var Ship =3  
+// document.write("price of item 1 is"+Pri +"<br/>" +"price of item 2 is"+Pric +"<br/>" +"Quantity of item 1 is"+Or +"<br/>" +"Quantity of item 2 is"+Ord +"<br/>"+"Shipping Chartges is"+Ship +"<br/>")
+// document.write("Total Coast Of Your Order "+eval(Pri*Pric*Or*Ord+Ship))
+
+
+var today =new Date()
+var year = today.getFullYear()
+var birthday= new Date('1 aug  ,1999')
+var birthdayyear =birthday.getFullYear() 
+var age = year - birthdayyear
+document.write('current years ' + year+'</br>')
+document.write('birthday year ' +birthdayyear +'</br>')
+document.write('Age' +age)
+
